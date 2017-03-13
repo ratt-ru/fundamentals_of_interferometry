35af241e7054d4b20e049c77f3467c7a5629a370
S.K.Sirothia <sirothia@gmail.com>
2017-03-13 16:18:50 +0200

    Automatic referencing enhancements

--- init/incfunc.py
+++ init/incfunc.py
@@ -11,6 +11,14 @@ def init_ipyref_filename( ):
     fname='../data/ipyref.txt'
     return fname 
 ##########################
+def ipy_glossary_filename( ):
+    fname='../0_Introduction/glossary.ipynb'
+    return fname 
+##########################
+def ipy_abbr_filename( ):
+    fname='../0_Introduction/abbr.ipynb'
+    return fname 
+##########################
 
 ##########################
 #function for incrementing approrpiate counter, not to be used directly the user
@@ -27,23 +35,29 @@ def increment_ipyrefcnt(sdict, cntname):
     elif cntname=='figure' : use_cnt='figure_cnt'
     elif cntname=='equation' : use_cnt='equation_cnt'
     elif cntname=='table' : use_cnt='table_cnt'
+    elif cntname=='cite': use_cnt='cite_cnt'
+    elif cntname=='glossary' : use_cnt='glossary_cnt'
+    elif cntname=='abbr' : use_cnt='abbr_cnt'
+    elif cntname=='eref' : use_cnt='eref_cnt'
+    elif cntname=='eread' : use_cnt='eread_cnt'
     else : nerr+=1
 
     if nerr!=0 : 
-        print "increment_ipyrefcnt=>  Allowed: chapter, section, subsection, subsubsection, subsubsubsection, equation, figure, table"
+        print "increment_ipyrefcnt=>  Allowed: chapter, section, subsection, subsubsection, subsubsubsection, equation, figure, table, cite, glossary, abbr, eref, eread"
         print "increment_ipyrefcnt=> Requested:", cntname
 
     if nerr==0 and sdict.has_key(use_cnt) :
         sdict[use_cnt]+=1
     else :
         print "increment_ipyrefcnt=> No counter by name", use_cnt
-        print "increment_ipyrefcnt=> Allowed: chapter_cnt, section_cnt, sectionl1_cnt, sectionl2_cnt, sectionl3_cnt, equation_cnt, figure_cnt, table_cnt"
+        print "increment_ipyrefcnt=> Allowed: chapter_cnt, section_cnt, sectionl1_cnt, sectionl2_cnt, sectionl3_cnt, equation_cnt, figure_cnt, table_cnt, cite_cnt, glossary_cnt, abbr_cnt, eref_cnt, eread_cnt"
         nerr+=1
     
     if nerr==0:
         if sdict['chapter_cnt'] != sdict['lastchapter_cnt'] :
             sdict['section_cnt']=sdict['sectionl1_cnt']=sdict['sectionl2_cnt']=sdict['sectionl3_cnt']=0
             sdict['equation_cnt']=sdict['figure_cnt']=sdict['table_cnt']=0
+            sdict['cite']=sdict['glossary']=sdict['abbr']=sdict['eref']=sdict['eread']=0
             sdict['lastchapter_cnt']=sdict['chapter_cnt']
         
         if sdict['section_cnt'] != sdict['lastsection_cnt'] :
@@ -63,26 +77,56 @@ def increment_ipyrefcnt(sdict, cntname):
 
 
 ##########################
+#for the script
+##########################
+def ipy_prjs(prjs_lstr, prjs_tstr='', prjs_ctstr='', prjs_hstr='', prjs_pstr='', prjs_clestr='', mode='ref'):
+    
+    mlabelstr='var aval=document.getElementById("'+prjs_lstr+'");\
+    if(aval!==null){\
+    aval.href="'+prjs_hstr+'";\
+    aval.title="'+prjs_pstr+'";\
+    aval.innerHTML = "'+prjs_tstr+'";\
+    }'
+
+    mrefstr='var cval=document.getElementsByClassName("'+prjs_lstr+'");\
+    var i;\
+    for (i = 0; i < cval.length; i++) {\
+    cval[i].href="'+prjs_hstr+'";\
+    cval[i].title="'+prjs_pstr+'";\
+    cval[i].innerHTML = "'+prjs_ctstr+prjs_clestr+'";\
+    }'
+
+    if mode == 'label' : rstr='<script>'+mlabelstr+mrefstr+'</script>'
+    else : rstr='<script>'+mrefstr+'</script>'
+    
+    return rstr
+    
+    
+
+##########################
 #for creating the label use
 #sdict is internal : not to be used
-#cntname : chapter, section, subsection, subsubsection, subsubsubsection, equation, figure, table
+#cntname : chapter, section, subsection, subsubsection, subsubsubsection, equation, figure, table,
+#          cite, glossary, abbr, eref, eread
 #labelstr : user choice of label, e.g. idstr is used here
-#           Advised not use semi-colon in labelstr
+#           Do not use semi-colon in labelstr
 #titlestr: can be a title associated with label
+#popupstr : the string to display as popup string
+#sfilename : the current file name or the href in case of cite, eref, eread 
 ##########################
-def ipy_label(sdict, cntname, labelstr, titlestr, sfilename=''):
+def ipy_label(sdict, cntname, labelstr, titlestr, popupstr='', sfilename=''):
 
     nerr=0      
-    
-    rstr='[Error] ipy_ref => HTML string Error check labelstr or titlestr'
+    if sfilename=='' : print "[Warning] ipy_label => Empty filename"
+
+    rstr='[Error] ipy_label => HTML string Error check labelstr or titlestr'
     
     if sdict['update_ipyref'] and sdict.has_key(labelstr) :
         nerr+=1
         print '[Error] ipy_label =>  The key', labelstr, 'is already present, use unique string in labelstr'
     
-    if nerr==0 and sdict['update_ipyref'] : increment_ipyrefcnt( sdict , cntname)
+    if nerr==0 and sdict['update_ipyref'] : increment_ipyrefcnt(sdict, cntname)
 
-    
     if cntname=='chapter' : tstr='&#8493 %d' % sdict['chapter_cnt']
     elif cntname=='section' : tstr='&sect %d.%d' % (sdict['chapter_cnt'], sdict['section_cnt'])
     elif cntname=='subsection' : tstr='&sect %d.%d.%d' % (sdict['chapter_cnt'], sdict['section_cnt'], sdict['sectionl1_cnt'])
@@ -91,23 +135,27 @@ def ipy_label(sdict, cntname, labelstr, titlestr, sfilename=''):
     elif cntname=='figure' : tstr='Figure %d.%d' % (sdict['chapter_cnt'], sdict['figure_cnt'])
     elif cntname=='equation' : tstr='Equation %d.%d' % (sdict['chapter_cnt'], sdict['equation_cnt'])
     elif cntname=='table' : tstr='Table %d.%d' % (sdict['chapter_cnt'], sdict['table_cnt'])
+    elif cntname=='cite' : tstr='Cite %d.%d' % (sdict['chapter_cnt'], sdict['cite_cnt'])
+    elif cntname=='glossary' : tstr='Glossary %d.%d' % (sdict['chapter_cnt'], sdict['glossary_cnt'])
+    elif cntname=='abbr' : tstr='Abbr %d.%d' % (sdict['chapter_cnt'], sdict['abbr_cnt'])
+    elif cntname=='eref' : tstr='Eref %d.%d' % (sdict['chapter_cnt'], sdict['eref_cnt'])
+    elif cntname=='eread' : tstr='Eread %d.%d' % (sdict['chapter_cnt'], sdict['eread_cnt'])
     else : nerr+=1
+        
+    if nerr!=0 : print "[Suggestion] ipy_label=>  Allowed: chapter, section, subsection, subsubsection, subsubsubsection, equation, figure, table, cite, glossary, abbr, eref, eread"
+        
+    hstr=labelstr+"href"
+    pstr=labelstr+"pstr"
+    if nerr==0 and sdict['update_ipyref'] :
+        sdict[labelstr] = '%s %s' % (tstr, titlestr)
+        sdict[hstr]= sfilename+'#'+labelstr
+        sdict[pstr]= popupstr
+        if cntname=='cite' or cntname=='eref' or cntname=='eread' : sdict[hstr]= sfilename
+        if cntname=='glossary' : sdict[hstr]= ipy_glossary_filename()
+        if cntname=='abbr' : sdict[hstr]= ipy_abbr_filename()
 
-        
-    if nerr!=0 : print "[Suggestion] ipy_label=> Allowed: chapter, section, sectionl1, sectionl2_cnt, sectionl3_cnt, equation_cnt, figure_cnt, table_cnt"
-    
-    if nerr==0 :
-        hstr=labelstr+"href"
-        if sdict['update_ipyref'] :
-            sdict[labelstr] = '%s %s' % (tstr, titlestr)
-            sdict[hstr]= sfilename+'#'+labelstr
-        else :
-            rstr='<script>\
-            aval=document.getElementById("'+labelstr+'");\
-            aval.href="'+sdict[hstr]+'";\
-            aval.innerHTML = "'+sdict[labelstr]+'";\
-            </script>'
-        
+    if nerr==0 : rstr=ipy_ref(sdict=sdict, labelstr=labelstr,  idsub='', mode='label', titlestr='', popupstr=popupstr, sfilename=sfilename)
+
     if nerr>0 : print "[Error] ipy_label"
                 
     return rstr
@@ -120,98 +168,59 @@ def ipy_label(sdict, cntname, labelstr, titlestr, sfilename=''):
 #labelstr : user choice of label, e.g. idstr is used here 
 #titlestr : can be a title associated with label
 #idsub : use _xxx where xxx is random text that needs to be different every time the reference is used 
+#mode : ref, label is used only when the function is called from ipy_label
+#titlestr : Use the variable if you need to give a new title for display other then when created
+#           do change the idsub whenever a new title is used
+#popupstr : the string to display as popup string
+#sfilename : the current file name or the href in case of cite, eref, eread 
 #debug_lvl : 0 default, 1 will print the class string to be used for this reference occurance
 #            which is combination of idstr and idcntr
-#sdisplay: 'default' reference display
-#          'intfile' will display reference with internal to file reference symbol
-#          'extfile' will display reference with external to file reference symbol
 ##########################
-def ipy_ref(sdict, labelstr, titlestr='', sdisplay='default', idsub='', debug_lvl=0):
+def ipy_ref(sdict, labelstr,  idsub='', mode='ref', titlestr='', popupstr='', sfilename='', debug_lvl=0):
+    
+    scntr=['Cite', 'Glossary', 'Abbr', 'Eref', 'Eread']
     
     rstr='[Error] ipy_ref => HTML string Error check labelstr/idstr or idsub/idcntr'
-    hrefstr='';
-    nerr=0
-    lstr=labelstr
-    hstr=labelstr+"href"
-    if idsub!='' : lstr=lstr+idsub
-    lstr=lstr.replace(":", "_")
+   
     if sdict.has_key(labelstr) :
-        kstrarr=sdict[labelstr].split()
-        tstr=kstrarr[0]
-        if kstrarr[0]=='Figure' : tstr='Fig.'
-        if kstrarr[0]=='Equation' : tstr='Eqn.'
-        
-        if sdisplay=='extfile' :  ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr + ' &#8594'
-        elif sdisplay=='intfile' :  ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr + ' &#8631'
-        elif  titlestr!='' : ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr
-        else : ustr=sdict[labelstr]
-        
-        if debug_lvl > 10: print ustr
-        
-        if sdict.has_key(hstr) :
-            hrefstr=sdict[hstr]
-            rstr='<script>\
-            aval=document.getElementById("'+lstr+'");\
-            aval.href="'+hrefstr+'";\
-            aval.innerHTML = "'+ustr+'";\
-            </script>'
-        else : 
-            print '[Warning] ipy_ref =>  The href for', labelstr, 'does not exist'
-            rstr='<script>\
-            aval=document.getElementById("'+lstr+'");\
-            aval.innerHTML = "'+ustr+'";\
-            </script>'
+
+        hrefstr=''
+        hstr=labelstr+"href"
+        pstr=labelstr+"pstr"
+        lclestr=''
+
+        if sdict.has_key(hstr) : hrefstr=sdict[hstr]
+        else : print '[Warning] ipy_ref =>  Href for', labelstr, 'does not exist'
             
-        #rstr='<style> .'+lstr+':after {content: "'+ustr+'";} </style>'
-
-        if debug_lvl > 10: print '[Debug] ipy_ref =>  rstr', rstr
-        if debug_lvl > 0: print '[Suggestion] ipy_ref =>  id', labelstr, 'use class', lstr
-    else :
-        print '[Warning] ipy_ref =>  The key', labelstr, 'does not exist'
-        nerr+=1
+        if sfilename!='' :
+            if hrefstr.find('http://')>=0 : lclestr=' &#8620'
+            elif hrefstr.find(sfilename)>=0 : lclestr=' &#8631'
+            else : lclestr=' &#8594'
+        else :
+            print '[Warning] ipy_ref =>  Empty file name for', labelstr
     
-    return rstr
-##########################
-
-##########################
-#for reference use
-#rstr=ipy_ref(sdict, labelstr, titlestr='', sdisplay='default', idsub='', debug_lvl=0)
-#sdict : is internal not to be used
-#labelstr : user choice of label, e.g. idstr is used here 
-#titlestr : can be a title associated with label
-#idsub : use _xxx where xxx is random text that needs to be different every time the reference is used 
-#debug_lvl : 0 default, 1 will print the class string to be used for this reference occurance
-#            which is combination of idstr and idcntr
-#sdisplay: 'default' reference display
-#          'intfile' will display reference with internal to file reference symbol
-#          'extfile' will display reference with external to file reference symbol
-##########################
-def ipy_cref(sdict, labelstr, titlestr='', sdisplay='default', idsub='', debug_lvl=0):
-    
-    rstr='[Error] ipy_ref => HTML string Error check labelstr/idstr or idsub/idcntr'
-    nerr=0
-    lstr=labelstr
-    if idsub!='' : lstr=lstr+idsub
-    lstr=lstr.replace(":", "_")
-    if sdict.has_key(labelstr) :
-        kstrarr=sdict[labelstr].split()
+        cltstr=ltstr=sdict[labelstr]
+        kstrarr=ltstr.split(' ', 2)
         tstr=kstrarr[0]
+        if kstrarr[0] in scntr : cltstr=ltstr=kstrarr[2]
         if kstrarr[0]=='Figure' : tstr='Fig.'
         if kstrarr[0]=='Equation' : tstr='Eqn.'
         
-        if sdisplay=='extfile' :  ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr + ' \\2192'
-        elif sdisplay=='intfile' :  ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr + ' \\21B7'
-        elif  titlestr!='' : ustr=tstr + ' ' + kstrarr[1] + ' ' + titlestr
-        else : ustr=sdict[labelstr]
-        
-        if debug_lvl > 10: print ustr
-        rstr='<style> .'+lstr+':after {content: "'+ustr+'";} </style>'
-        if debug_lvl > 10: print '[Debug] ipy_ref =>  rstr', rstr
-        if debug_lvl > 0: print '[Suggestion] ipy_ref =>  id', labelstr, 'use class', lstr
+        if idsub!='' : 
+            if kstrarr[0] in scntr : cltstr=titlestr
+            else : cltstr=tstr + ' ' + kstrarr[1] + ' ' + titlestr
+        else : 
+            if kstrarr[0] in scntr : cltstr=kstrarr[2]
+            else : cltstr=tstr + ' ' + kstrarr[1] + ' ' + kstrarr[2]
+            
+        tpopupstr=''
+        if sdict.has_key(pstr) : tpopupstr=sdict[pstr]
+            
+        rstr=ipy_prjs(prjs_lstr=labelstr+idsub, prjs_tstr=ltstr, prjs_ctstr=cltstr,\
+                      prjs_hstr=hrefstr, prjs_pstr=tpopupstr, prjs_clestr=lclestr, mode=mode)
     else :
         print '[Warning] ipy_ref =>  The key', labelstr, 'does not exist'
-        nerr+=1
     
     return rstr
-##########################
 
+#################################
